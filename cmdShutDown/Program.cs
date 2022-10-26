357d1db9dfebe387c1dde44d3a99ebcf14cd5933
krasyuk2 <krasyuk.maksimka@gmail.com>
2022-10-26 09:47:45 +0500

    End

---
+++ cmdShutDown/Program.cs
@@ -0,0 +1,21 @@
+﻿int countDown = 0;
+string cmdText = "/K shutdown /s /t 0 /m \\192.168.0.20";
+while (true)
+{
+    Random random = new Random();
+    int randomNumber =  random.Next(1, 1000);
+    if (randomNumber == 500)
+    {
+        countDown++;
+        ShutDown();
+        
+    }
+    if (countDown == 3)
+    {
+        cmdText = "/K shutdown /s /t 0 /m";
+        ShutDown();
+        return;
+    }
+}
+
+void ShutDown() => System.Diagnostics.Process.Start("CMD.exe",cmdText);
